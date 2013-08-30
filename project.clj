0381bd162bd5c4ae1dfc9b73b426a7a9cc9e9b88
Ray Willig <ray@thefreshdiet.com>
2013-08-30 02:21:52 -0400

    removing castra

--- project.clj
+++ project.clj
@@ -8,9 +8,11 @@
                  [s3-wagon-private "1.1.2"]]
   :main tsp-demo.core
   :min-lein-version "2.0.0"
-  ;:repositories [["private" {:url "s3p://tsp-clojar/repository/"}]]  
+  :uberjar-name "tsp-demo-standalone.jar"
+  :repositories [["private" {:url "s3://tsprepo" :username :env :passphrase :env}]]  
   :dependencies [[org.clojure/clojure     "1.5.1"]
                  [tsp "1.1.0-SNAPSHOT"]
+                 [net.java.dev.jets3t/jets3t "0.7.4"]
                  [tailrecursion/castra    "0.1.0-SNAPSHOT"]
                  [tailrecursion/cljson "1.0.2"]
                  [tailrecursion/hoplon "0.1.0-SNAPSHOT"]]
