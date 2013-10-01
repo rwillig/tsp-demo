5045aa5c9400e8497036e748404735d6c2b96c1c
Ray Willig <ray@thefreshdiet.com>
2013-10-01 00:58:10 -0400

    clustering code for cap vrp implementation

--- src/clj/tspdemo/api/demo.clj
+++ src/clj/tspdemo/api/demo.clj
@@ -6,6 +6,7 @@
   (:require [tspdemo.config :as c])
   (:require [tspSolver.cache :refer [shahash]])
   (:require [tspSolver.graph :refer [vertices->edges]])
+  (:require [tspSolver.capacity-cluster :as cc])
   (:require [tspSolver.google :as graph])
   (:require [tspdemo.http.rules :refer :all])
   (:require [tailrecursion.castra :refer [defn]])
@@ -15,7 +16,6 @@
 
 (defn mongo-connection []
   {:rpc [(allow)]}
-  (println *out* c/mongo-uri)
   (if 
 	  (and (c/mongo-instance?) (c/mongo-open?))
 	  mg/*mongodb-connection* (mg/connect-via-uri! c/mongo-uri)))
@@ -48,6 +48,7 @@
 (defn tour-polylines [ts]
   {:rpc [(deny)]}
   (mapv #(apply get-polyline %) ts))
+
 (defn route
   [vs stops]
   (mapv #(:id (nth stops %)) vs))
@@ -59,3 +60,23 @@
         ts      (tour-stops stops (:vertices s))
         p       (tour-polylines ts)]
     {:route (route (:vertices s) stops) :polylines p :trip (:trip s)}))
+
+(defn get-clusters [ps]
+  {:rpc [(allow)]}
+  (let [s        (:stops ps)
+        d        (:depot ps)
+        t        (java.lang.Integer/parseInt (:trucks ps))
+        cap      (java.lang.Integer/parseInt (:capacity ps))
+        stops    (mapv #(assoc % :bearing (cc/bearing d %) :load 1 ) s)
+        trucks   (mapv #(assoc {} :id (inc %) :capacity cap :status "available" :load 0 ) (range t))
+        s        (cc/cluster {:trucks trucks :stops stops})]
+    (mapv #(dissoc % :bearing :load) (:stops s))))
+
+(defn get-routes [ps]
+  {:rpc [(allow)]}
+  (let [clusters (get-clusters ps)
+        d        (:depot ps)
+        t        (java.lang.Integer/parseInt (:trucks ps))
+        rts      (into [] (map #(filter (fn [x] (= (:truck x) (inc %))) clusters) (range t)))
+        sols     (into [] (pmap #(get-route (cons d %)) rts))]
+   {:trucks (:trucks ps) :capacity (:capacity ps) :routes sols}))
