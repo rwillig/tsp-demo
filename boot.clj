86a971a810e8fef37099d0d937748f3f001b2c83
Ray Willig <ray@thefreshdiet.com>
2013-10-06 16:39:09 -0400

    support for run task to launch jetty server

--- boot.clj
+++ boot.clj
@@ -1,13 +1,17 @@
 {:project         tsp-demo 
  :version         "0.1.0-SNAPSHOT"
- :src-paths       #{"src/html" "src/clj"}
+ :src-paths       #{"src/html" "src/clj" "src/static"}
  :dependencies    [
                   [tailrecursion/boot.task "0.1.0-SNAPSHOT"]
                   [tailrecursion/hoplon "1.1.0-SNAPSHOT"]
                   [tsp "1.1.2-SNAPSHOT"]
+                  [ring-middleware-index-file "1.0.5-SNAPSHOT"]
                   ]
  :src-static  #{"src/static"}
  :html-out "resources/public"
  :require-tasks #{[tailrecursion.boot.task :as t]
                   [tailrecursion.hoplon.boot :as h]
-                  }} 
+                  }
+ :tasks {:run {:doc "Start tsp demo server (port 3000)."
+               :main [tspdemo.run/run-task]}}
+ } 
--- src/clj/tspdemo/run.clj
+++ src/clj/tspdemo/run.clj
@@ -1,9 +1,7 @@
 (ns tspdemo.run
   (:gen-class)
   (:require
-    [clojure.pprint                  :refer [pprint]]
     [ring.adapter.jetty              :as jetty]
-    ;[compojure.route                 :as route]
     [ring.middleware.resource        :refer [wrap-resource]]
     [ring.middleware.session         :refer [wrap-session]]
     [ring.middleware.session.cookie  :refer [cookie-store]]
@@ -13,30 +11,22 @@
     [tailrecursion.castra.handler    :refer [castra]]))
 
 
-(comment (defn get-app [servlet]
-  ;(pprint servlet)
-  (let [app (->
-    (castra '[clojure.core :only [inc]] 'tspdemo.api.demo)
-    (wrap-session {:store (cookie-store {:key "a 16-byte secret"})})
-    (wrap-index-paths "public")    
-    ;(wrap-resource "public")
-    ;(wrap-file "public" {:index-files? true})
-    (wrap-file-info))] app )))
-
-
 (def app
   (->
-    (castra '[clojure.core :only [inc]] 'tspdemo.api.demo)
+    (castra  'tspdemo.api.demo)
     (wrap-session {:store (cookie-store {:key "a 16-byte secret"})})
     (wrap-resource "public")    
     (wrap-index-paths "index.html")    
     (wrap-file-info)))
 
-;(defonce server (jetty/run-jetty #'app {:join? false :port 3000}))
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (let [app app] (jetty/run-jetty app {:join? false :port 3000})))
+  (jetty/run-jetty #'app {:join? false :port 3000}))
 
-  
+(defn run-task [boot]
+  (-main)
+  (fn [continue]
+    (fn [event]
+      (continue event))))  
 
